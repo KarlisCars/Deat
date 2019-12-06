736dd5e119b9d963ae5a3154970b15b335214ae6
Karlis Cars <karlis.cars@gmail.com>
2019-12-06 15:20:49 +0200

    Initial Commit

---
+++ Deat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Deat
+//
+//  Created by Karlis Cars on 06/12/2019.
+//  Copyright © 2019 Karlis Cars. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
